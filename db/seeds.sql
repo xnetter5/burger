ad0d7f5db70c38aa5be95d9c2bf211e952188a41
Alexander Netter <xan@AlexandersMBP2.lan1>
2018-11-08 23:53:15 -0500

    burger

---
+++ db/seeds.sql
@@ -0,0 +1,5 @@
+USE `burgers_db`;
+
+INSERT INTO burgers (burger_name, devoured, date) VALUES ('Angus', false);
+INSERT INTO burgers (burger_name, devoured, date) VALUES ('Salmon', false);
+INSERT INTO burgers (burger_name, devoured, date) VALUES ('Mushroom', false);
